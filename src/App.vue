cc647d5521cce1a0095fa880a57d7b214f52e9d9
David Hulme <dhulme@outlook.com>
2017-11-11 15:52:01 +0000

    Tap tempo

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,7 @@
         <v-btn color="primary" large @click="toggleRunning">
           {{ running ? 'Stop' : 'Start' }}
         </v-btn>
+        <v-btn large @click="tapTempo">Tap tempo</v-btn>
       </v-container>
     </main>
   </v-app>
@@ -16,6 +17,8 @@
 <script>
   const metronomeSound = new Audio('./static/metronome.wav');
   const metronomeUpSound = new Audio('./static/metronome-up.wav');
+
+  const TAP_TEMPO_RESET_TIME = 2000;
 
   export default {
     data() {
@@ -27,6 +30,9 @@
         time: performance.now(),
         timeSignature: '4/4',
         soundReady: false,
+        tapTempoTimes: [],
+        tapTempoTime: null,
+        tapTempoLastTime: null,
       };
     },
     computed: {
@@ -38,28 +44,28 @@
       },
       measure() {
         return Number(this.timeSignature.split('/')[1]);
-      }
+      },
     },
     watch: {
       bpm: 'reset',
       beats: 'reset',
-      measure: 'reset'
+      measure: 'reset',
     },
     mounted() {
       const frame = () => {
         const d = performance.now() - this.time;
         if (d / this.totalCount > this.interval) {
           this.tick();
-          this.totalCount++;
+          this.totalCount += 1;
         }
         requestAnimationFrame(frame);
-      }
+      };
       requestAnimationFrame(frame);
     },
     methods: {
       toggleRunning() {
         this.running = !this.running;
-        
+
         if (this.running) {
           metronomeUpSound.play();
           setTimeout(() => metronomeSound.play(), this.interval);
@@ -87,9 +93,30 @@
         } else {
           metronomeSound.play();
         }
-        this.count++;
-      }
-    }
+        this.count += 1;
+      },
+      tapTempo() {
+        if (!this.running) {
+          this.toggleRunning();
+        }
+
+        const now = performance.now();
+        if (!this.tapTempoTime || (now - TAP_TEMPO_RESET_TIME > this.tapTempoTime)) {
+          this.tapTempoTimes = [];
+        } else if (this.tapTempoTime) {
+          this.tapTempoTimes.push(now - this.tapTempoTime);
+        }
+        this.tapTempoTime = now;
+
+        const averageTime = (this.tapTempoTimes.reduce((acc, tap) => {
+          return acc + (tap - this.tapTempoLastTime);
+        }, 0) / this.tapTempoTimes.length) || 0;
+
+        if (averageTime !== 0) {
+          this.bpm = Math.round((60 * 1000) / averageTime) / (this.measure / 4);
+        }
+      },
+    },
   };
 </script>
 
